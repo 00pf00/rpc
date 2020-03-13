bc821713ed2a1166dd3bd4ef505db4296f4f0a61
feeli <feeli@tencent.com>
2020-03-13 10:54:03 +0800

    debug server disconnect

--- consumer/src/main/java/cn/bupt/edu/consumer/channel/Client.java
+++ consumer/src/main/java/cn/bupt/edu/consumer/channel/Client.java
@@ -10,7 +10,7 @@ public class Client {
 
     public static void Start() {
         ClientThreadPool.initThreadPool();
-        client = ClientManagement.RegisterChannelClient("127.0.0.1", 7000);
+        client = ClientManagement.RegisterChannelClient("10.201.0.145", 7000);
     }
 
     public static Channel getChannel() {
