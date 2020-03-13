a0560311b0b7e7c37acf5c809d7e61bd961c9f49
feeli <feeli@tencent.com>
2020-03-13 18:10:27 +0800

    debug reconnect

--- server/src/main/java/cn/bupt/edu/server/netty/handler/HeartBeatHandler.java
+++ server/src/main/java/cn/bupt/edu/server/netty/handler/HeartBeatHandler.java
@@ -32,11 +32,7 @@ public class HeartBeatHandler extends ChannelInboundHandlerAdapter {
 
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         System.out.println("inbound 自定义事件\n");
-        ctx.close();
-    }
-
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("HeartBeatHandler channelInactive\n");
         ctx.fireChannelInactive();
     }
+
 }
