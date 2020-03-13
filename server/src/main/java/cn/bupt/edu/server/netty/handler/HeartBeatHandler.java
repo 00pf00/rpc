57198b9a8bb80a15762a21ee90433e764252ed9c
feeli <feeli@tencent.com>
2020-03-13 12:18:47 +0800

    debug server readtimeout

--- server/src/main/java/cn/bupt/edu/server/netty/handler/HeartBeatHandler.java
+++ server/src/main/java/cn/bupt/edu/server/netty/handler/HeartBeatHandler.java
@@ -29,10 +29,4 @@ public class HeartBeatHandler extends ChannelInboundHandlerAdapter {
         }
 
     }
-
-    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        System.out.println("inbound 自定义事件\n");
-        ctx.fireChannelUnregistered();
-    }
-
 }
